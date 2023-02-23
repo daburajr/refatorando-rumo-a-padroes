2fb437d966bb4cce915d42c154247c7de98b02f7
Jock R <jockrodrigues8@gmail.com>
2023-02-23 18:55:08 -0300

    Removendo imports nao utilizados

--- src/main/java/br/com/ifma/refatorandorumoapadroes/facade/service/UsuarioService.java
+++ src/main/java/br/com/ifma/refatorandorumoapadroes/facade/service/UsuarioService.java
@@ -3,20 +3,11 @@ package br.com.ifma.refatorandorumoapadroes.facade.service;
 import br.com.ifma.refatorandorumoapadroes.facade.dto.UsuarioDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Slf4j
 @Service
