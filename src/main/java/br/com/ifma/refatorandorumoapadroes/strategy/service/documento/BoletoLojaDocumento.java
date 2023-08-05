6ad8bbf369665ac91edefff03c7c6a8a1b45c486
Jock R <jockrodrigues8@gmail.com>
2023-08-05 18:26:49 -0300

    refactor: segunda refatoracao rumo ao strategy

--- src/main/java/br/com/ifma/refatorandorumoapadroes/strategy/service/documento/BoletoLojaDocumento.java
+++ src/main/java/br/com/ifma/refatorandorumoapadroes/strategy/service/documento/BoletoLojaDocumento.java
@@ -1,20 +1,32 @@
 package br.com.ifma.refatorandorumoapadroes.strategy.service.documento;
 
 import br.com.ifma.refatorandorumoapadroes.strategy.enumeration.TipoBoleto;
+import br.com.ifma.refatorandorumoapadroes.strategy.enumeration.TipoStatusImpressao;
 import br.com.ifma.refatorandorumoapadroes.strategy.model.BoletoItMarket;
 
 import java.util.List;
+
+import static br.com.ifma.refatorandorumoapadroes.strategy.enumeration.TipoBoleto.*;
 
 public class BoletoLojaDocumento implements Documento {
 
     @Override
     public boolean executaProcessamento(TipoBoleto tipo) {
-        return false;
+        return BOLETO_LOJA.equals(tipo);
     }
 
     @Override
     public void imprime(List<BoletoItMarket> documentos) {
-
+        documentos.forEach(boletoItMarket -> {
+            try {
+                boletoReports.imprimirBoletoLoja(boletoItMarket);
+                this.atualizarBoletoItMarket(boletoItMarket,
+                        TipoStatusImpressao.IMPRESSAO_CONCLUIDA);
+            } catch (Exception e) {
+                this.registrarIncidenciaEError(boletoItMarket,
+                        e.getMessage());
+            }
+        });
     }
 
 }
