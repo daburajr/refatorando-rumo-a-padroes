07ff65aca3a5aa4b39fb36de2d29391fde1da850
Jock R <jockrodrigues8@gmail.com>
2023-08-06 17:59:32 -0300

    refactor: segunda refatoracao rumo ao template method

--- src/main/java/br/com/ifma/refatorandorumoapadroes/strategy/service/documento/BoletoLojaDocumento.java
+++ src/main/java/br/com/ifma/refatorandorumoapadroes/strategy/service/documento/BoletoLojaDocumento.java
@@ -2,70 +2,19 @@ package br.com.ifma.refatorandorumoapadroes.strategy.service.documento;
 
 import br.com.ifma.refatorandorumoapadroes.strategy.client.IBancoCupomClient;
 import br.com.ifma.refatorandorumoapadroes.strategy.client.IBoletoReports;
-import br.com.ifma.refatorandorumoapadroes.strategy.enumeration.TipoBoleto;
-import br.com.ifma.refatorandorumoapadroes.strategy.enumeration.TipoStatusImpressao;
 import br.com.ifma.refatorandorumoapadroes.strategy.mapper.BoletoImpressaoMapper;
-import br.com.ifma.refatorandorumoapadroes.strategy.model.BoletoItMarket;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-import static br.com.ifma.refatorandorumoapadroes.strategy.enumeration.TipoBoleto.*;
 
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
-public class BoletoLojaDocumento implements Documento {
+public class BoletoLojaDocumento extends TemplateDocumento {
 
-    private final BoletoImpressaoMapper boletoImpressaoMapper;
-    private final IBoletoReports boletoReports;
-    private final IBancoCupomClient cupomCapaService;
 
-    private static final Integer INCIDENCIA = 15;
-
-    @Override
-    public boolean executaProcessamento(TipoBoleto tipo) {
-        return BOLETO_LOJA.equals(tipo);
+    protected BoletoLojaDocumento(BoletoImpressaoMapper boletoImpressaoMapper,
+                                  IBoletoReports boletoReports,
+                                  IBancoCupomClient cupomCapaService) {
+        super(boletoImpressaoMapper, boletoReports, cupomCapaService);
     }
-
-    @Override
-    public void imprime(List<BoletoItMarket> documentos) {
-        documentos.forEach(boletoItMarket -> {
-            try {
-                boletoReports.imprimirBoletoLoja(boletoItMarket);
-                this.atualizarBoletoItMarket(boletoItMarket,
-                        TipoStatusImpressao.IMPRESSAO_CONCLUIDA);
-            } catch (Exception e) {
-                this.registrarIncidenciaEError(boletoItMarket,
-                        e.getMessage());
-            }
-        });
-    }
-
-    private void registrarIncidenciaEError(BoletoItMarket boletoItMarket,
-                                           String mensagemDeErro) {
-
-        boletoItMarket.adicionaIncidencia();
-
-        if (boletoItMarket.getIncidencia() >= INCIDENCIA) {
-
-            this.atualizarBoletoItMarket(boletoItMarket,
-                    TipoStatusImpressao.IMPRESSAO_COM_ERRO);
-
-            boletoImpressaoMapper
-                    .registrarError(boletoItMarket, mensagemDeErro);
-        }
-
-        boletoImpressaoMapper.atualizarBoletoItMarket(boletoItMarket);
-    }
-
-    private void atualizarBoletoItMarket(BoletoItMarket boletoItMarket,
-                                         TipoStatusImpressao statusImpressao) {
-        boletoItMarket.setTipoStatusImpressao(statusImpressao.getCodigo());
-        boletoImpressaoMapper.atualizarBoletoItMarket(boletoItMarket);
-    }
-
 }
--- src/main/java/br/com/ifma/refatorandorumoapadroes/strategy/service/documento/TemplateDocumento.java
+++ src/main/java/br/com/ifma/refatorandorumoapadroes/strategy/service/documento/TemplateDocumento.java
@@ -1,16 +1,72 @@
 package br.com.ifma.refatorandorumoapadroes.strategy.service.documento;
 
+import br.com.ifma.refatorandorumoapadroes.strategy.client.IBancoCupomClient;
+import br.com.ifma.refatorandorumoapadroes.strategy.client.IBoletoReports;
 import br.com.ifma.refatorandorumoapadroes.strategy.enumeration.TipoBoleto;
+import br.com.ifma.refatorandorumoapadroes.strategy.enumeration.TipoStatusImpressao;
+import br.com.ifma.refatorandorumoapadroes.strategy.mapper.BoletoImpressaoMapper;
 import br.com.ifma.refatorandorumoapadroes.strategy.model.BoletoItMarket;
 
 import java.util.List;
 
+import static br.com.ifma.refatorandorumoapadroes.strategy.enumeration.TipoBoleto.BOLETO_LOJA;
+
 public abstract class TemplateDocumento implements Documento {
 
-    @Override
-    public boolean executaProcessamento(TipoBoleto tipo) {}
+    private final BoletoImpressaoMapper boletoImpressaoMapper;
+    private final IBoletoReports boletoReports;
+    private final IBancoCupomClient cupomCapaService;
+
+    private static final Integer INCIDENCIA = 15;
+
+    protected TemplateDocumento(BoletoImpressaoMapper boletoImpressaoMapper,
+                                IBoletoReports boletoReports,
+                                IBancoCupomClient cupomCapaService) {
+        this.boletoImpressaoMapper = boletoImpressaoMapper;
+        this.boletoReports = boletoReports;
+        this.cupomCapaService = cupomCapaService;
+    }
 
     @Override
-    public void imprime(List<BoletoItMarket> documentos) {}
+    public boolean executaProcessamento(TipoBoleto tipo) {
+        return BOLETO_LOJA.equals(tipo);
+    }
 
+    @Override
+    public void imprime(List<BoletoItMarket> documentos) {
+        documentos.forEach(boletoItMarket -> {
+            try {
+                boletoReports.imprimirBoletoLoja(boletoItMarket);
+                this.atualizarBoletoItMarket(boletoItMarket,
+                        TipoStatusImpressao.IMPRESSAO_CONCLUIDA);
+            } catch (Exception e) {
+                this.registrarIncidenciaEError(boletoItMarket,
+                        e.getMessage());
+            }
+        });
+    }
+
+    private void registrarIncidenciaEError(BoletoItMarket boletoItMarket,
+                                           String mensagemDeErro) {
+
+        boletoItMarket.adicionaIncidencia();
+
+        if (boletoItMarket.getIncidencia() >= INCIDENCIA) {
+
+            this.atualizarBoletoItMarket(boletoItMarket,
+                    TipoStatusImpressao.IMPRESSAO_COM_ERRO);
+
+            boletoImpressaoMapper
+                    .registrarError(boletoItMarket, mensagemDeErro);
+        }
+
+        boletoImpressaoMapper.atualizarBoletoItMarket(boletoItMarket);
+    }
+
+    private void atualizarBoletoItMarket(BoletoItMarket boletoItMarket,
+                                         TipoStatusImpressao statusImpressao) {
+        boletoItMarket.setTipoStatusImpressao(statusImpressao.getCodigo());
+        boletoImpressaoMapper.atualizarBoletoItMarket(boletoItMarket);
+    }
 }
+
